f9637ec3ee70e6b039f8b32a00b1457361b5b9bd
sawangpong muadphet <sawangpongm@gmail.com>
2011-04-17 22:45:07 +0700

    commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Halq::Application.config.secret_token = '0d89ea22e785c46704354431a452713a7fdc6241748891d524dad7c2fd3e5727be4b752bf947df82afae5bbfd4aedd56b0de7b0684058059a5062ab52b95e756'
